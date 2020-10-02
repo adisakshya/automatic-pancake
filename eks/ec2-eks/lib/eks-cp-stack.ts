63d5c7db904dcf1c4426d156fe86a1be7c8f7d38
adisakshya <adisakshya@triveous.com>
2020-10-02 19:44:23 +0530

    eks/ec2 initial commit

--- eks/ec2-eks/lib/eks-cp-stack.ts
+++ eks/ec2-eks/lib/eks-cp-stack.ts
@@ -3,6 +3,14 @@ const ec2 = require('@aws-cdk/aws-ec2');
 const eks = require('@aws-cdk/aws-eks');
 const iam = require('@aws-cdk/aws-iam');
 
+/**
+ * AWS Managed Polciy ARNs
+ */
+const EKS_POLICIES: string[] = [
+  "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
+  "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+];
+
 export class EksCpStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -15,46 +23,46 @@ export class EksCpStack extends cdk.Stack {
       enableDnsSupport: true,
       subnetConfiguration: [
         {
-          name: 'eks-cp-subnet-01',
+          name: 'eks-cp-public-subnet-01',
           subnetType: ec2.SubnetType.PUBLIC,
           cidrMask: '192.168.64.0/18'
         },
         {
-          name: 'eks-cp-subnet-02',
+          name: 'eks-cp-private-subnet-01',
           subnetType: ec2.SubnetType.PRIVATE,
           cidrMask: '192.168.128.0/18',
         },
         {
-          name: 'eks-cp-subnet-03',
+          name: 'eks-cp-private-subnet-02',
           subnetType: ec2.SubnetType.PRIVATE,
           cidrMask: '192.168.192.0/18'
         }
       ]
     });
 
-    // Security group
+    // Roles
+    const eksRole = new iam.Role(this, 'EksServiceRole', {
+      assumedBy: new iam.ServicePrincipal('eks.amazonaws.com'),
+      managedPolicyArns: EKS_POLICIES
+    });
+
+    // Security groups
     const eksCpSg = new ec2.SecurityGroup(this, 'eks-cp-sg', {
       vpc: eksCpVpc,
       securityGroupName: 'eks-cp-ng-sg',
       description: 'Allow cluster communication with worker nodes'
     });
 
-    // Roles
-    const eksClusterAdmin = new iam.Role(this, 'eksClusterAdmin', {
-      assumedBy: new iam.AccountRootPrincipal()
-    });
-
     // Create cluster
-    const eksCluster = new eks.Cluster(this, 'eks-cluster', {
-      vpc: eksCpVpc,
-      mastersRole: eksClusterAdmin,
-      defaultCapacity: 0,
-      version: eks.KubernetesVersion.V1_16
-    });
+    // const cluster = new eks.Cluster(this, 'eks-cluster', {
+    //   version: eks.KubernetesVersion.V1_16
+    // });
 
-    // Stack outputs
+    // // Stack outputs
+    const publicSubnetIds = eksCpVpc.publicSubnets.map((s:any) => s.subnetId);
+    const privateSubnetIds = eksCpVpc.privateSubnets.map((s:any) => s.subnetId);
     const eksCpVpcId = this.stackOutput('EksCpVpcId', eksCpVpc.vpcId);
-    const eksCpSecurityGroupId = this.stackOutput('EksCpSecurityGroupId', eksCpSg.securityGroupId);
+    // const eksCpSecurityGroupId = this.stackOutput('EksCpSecurityGroupId', eksCpSg.securityGroupId);
     const eksClusterName = this.stackOutput('EksClusterName', eksCluster.clusterName);
   }
   
